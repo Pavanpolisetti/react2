5b4f6aadefb03389da53ed2209ebce27500a88f9
Pavanpolisetti <saipavankumarlyrostech.com>
2024-08-19 08:12:25 +0530

    first commit

--- src/Contactmanager/ContactManger2.js
+++ src/Contactmanager/ContactManger2.js
@@ -1,21 +1,45 @@
 import React from "react";
 import axios from "axios";
 class ContactManger2 extends React.Component{
-    constarcter(props){
+    constructor(props){
         super(props)
         this.state={
             value:[],
-            cid:"",
-            cname:""
+            cmail:"",
+            cname:"",
+            cnumb:""
         }
 
+    }
+    componentDidMount(){
+        axios.get("http://localhost:3001/contactdetails").then((res)=>this.setState({value:res.data})).catch((err)=>console.log("error"))
+    }
+    handleChange(e,keyword){
+        if(keyword==="ContactName"){
+            this.setState({cname:e.target.value})
+        }else if(keyword==="ContactNumber"){
+            this.setState({cnumb:e.target.value})
+        }else{
+            this.setState({cmail:e.target.value})
+        }
+
+    }
+    handleSubmit(e){
+        e.preventDefault()
+        axios.post("http://localhost:3001/contactdetails",{
+            c.name:
+        })
     }
     render(){
         return(
             <form>
-                contactName:<input type="text" placeholder="enter name"
+                ContactName:<input type="text" placeholder="enter name"  onChange={(e)=>this.handleChange(e,"ContactName")} ></input>
+                ContactNumber:<input type="number" placeholder="enter number" onChange={(e)=>this.handleChange(e,"ContactNumber")}></input>
+                ContactMail:<input type="mail" placeholder="entermail" onChange={(e)=>this.handleChange(e,"contactMail")}></input>
+                <button onClick={(e)=>this.handleSubmit(e)}>submit</button>
             </form>
 
         )
     }
 }
+export default ContactManger2
